b35eb537df3f9a183b227e8989fd6c3050bd6550
Max <meliqq98@gmail.com>
2025-04-22 10:48:15 +0200

    added env variable for host url

--- src/freegptFetcher.jsx
+++ src/freegptFetcher.jsx
@@ -1,7 +1,7 @@
 import ky from "ky";
 
 export const fetchWord = async (prompt) =>{
-    const url = 'http://127.0.0.2:8000/generate/?prompt='+prompt;
+    const url = `${import.meta.env.VITE_API_URL || "http://127.0.0.2:8000"}/generate/?prompt=${prompt}`;
     const json = await ky.get(url, {timeout: 120000}).json();
     return json
   }
